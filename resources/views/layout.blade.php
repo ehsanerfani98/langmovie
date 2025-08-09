108a06856ee89da025e48a529e6d6086348789bd
Realesi89 <ehsan.bavaghar1989@gmail.com>
2025-08-09 21:15:42 +0330

    add card-alert-subscribe to home page user

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -283,7 +283,7 @@
                                 </path>
                             </svg>
                             <span>۰۹۱۹-۱۸۱۶۱۷۲</span>
-                            <br>
+                            |
                             <span>۰۲۸-۳۲۵۲۶۶۳۶</span>
                         </li>
                         <li class="flex items-start gap-2">
