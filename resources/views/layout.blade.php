1b02323bbcbe0ae1c3b1ec8f2e8f7fc55d069b10
Realesi89 <ehsan.bavaghar1989@gmail.com>
2025-08-09 21:12:55 +0330

    add card-alert-subscribe to home page user

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -306,8 +306,7 @@
                             href='https://trustseal.enamad.ir/?id=634664&Code=FTsAnb9pdUE35gAA4AZYxDi3zsEkHDcP'><img
                                 referrerpolicy='origin'
                                 src='https://trustseal.enamad.ir/logo.aspx?id=634664&Code=FTsAnb9pdUE35gAA4AZYxDi3zsEkHDcP'
-                                alt='' style='cursor:pointer;'
-                                code='FTsAnb9pdUE35gAA4AZYxDi3zsEkHDcP'></a>
+                                alt='' style='cursor:pointer;' code='FTsAnb9pdUE35gAA4AZYxDi3zsEkHDcP'></a>
                         <div>
                             <script src="https://www.zarinpal.com/webservice/TrustCode" type="text/javascript"></script>
                         </div>
@@ -351,6 +350,23 @@
             menuOverlay.classList.remove('active');
             document.body.style.overflow = 'auto';
         });
+    </script>
+    <script>
+        ! function(t, e, n) {
+            t.yektanetAnalyticsObject = n, t[n] = t[n] || function() {
+                t[n].q.push(arguments)
+            }, t[n].q = t[n].q || [];
+            var a = new Date,
+                r = a.getFullYear().toString() + "0" + a.getMonth() + "0" + a.getDate() + "0" + a.getHours(),
+                c = e.getElementsByTagName("script")[0],
+                s = e.createElement("script");
+            s.id = "ua-script-GSCl4bYc";
+            s.dataset.analyticsobject = n;
+            s.async = 1;
+            s.type = "text/javascript";
+            s.src = "https://cdn.yektanet.com/rg_woebegone/scripts_v3/GSCl4bYc/rg.complete.js?v=" + r, c.parentNode
+                .insertBefore(s, c)
+        }(window, document, "yektanet");
     </script>
 </body>
 
--- resources/views/site/layout.blade.php
+++ resources/views/site/layout.blade.php
@@ -106,6 +106,23 @@
     </script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script src="{{ asset('site-assets/js/scripts.js') }}"></script>
+    <script>
+        ! function(t, e, n) {
+            t.yektanetAnalyticsObject = n, t[n] = t[n] || function() {
+                t[n].q.push(arguments)
+            }, t[n].q = t[n].q || [];
+            var a = new Date,
+                r = a.getFullYear().toString() + "0" + a.getMonth() + "0" + a.getDate() + "0" + a.getHours(),
+                c = e.getElementsByTagName("script")[0],
+                s = e.createElement("script");
+            s.id = "ua-script-GSCl4bYc";
+            s.dataset.analyticsobject = n;
+            s.async = 1;
+            s.type = "text/javascript";
+            s.src = "https://cdn.yektanet.com/rg_woebegone/scripts_v3/GSCl4bYc/rg.complete.js?v=" + r, c.parentNode
+                .insertBefore(s, c)
+        }(window, document, "yektanet");
+    </script>
     @stack('scripts')
 
 </body>
