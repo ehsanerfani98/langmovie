caae505c3c93c4f5361a7d93e4e6ad99a75937cb
Realesi89 <ehsan.bavaghar1989@gmail.com>
2025-08-09 14:17:29 +0330

    add card-alert-subscribe to home page user

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -15,7 +15,15 @@
         </div>
     @endsession
 
-
+    @if (count($errors) > 0)
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
 
     <x-sliders-list />
 
