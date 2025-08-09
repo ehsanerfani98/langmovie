cd0fd36de7e4946cadb1d7aa054a7c4fe3920d84
Realesi89 <ehsan.bavaghar1989@gmail.com>
2025-08-09 14:00:10 +0330

    add card-alert-subscribe to home page user

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -21,6 +21,13 @@
 
     <x-search-movie />
 
+    <div class="card-alert-subscribe">
+        <p>شما در حال حاضر از اشتراک رایگان استفاده میکنید</p>
+        <p>در حالت رایگان روزانه فقط ۳ عبارت را می توانید جستجو کرده و در هر بار جستجو فقط ۳ سکانس نمایش داده می شود!</p>
+        <p>برای استفاده نامحدود می توانید اشتراک حرفه ای تهیه کنید</p>
+        <p><a href="{{ route('user.subscript.plans') }}" class="btn btn-sm btn-light mb-3">خرید اشتراک</a></p>
+    </div>
+
     {{-- <x-services-list /> --}}
 
     <x-banner-list />
