ec4be4bfbca40d96746da454def33a624baa9b3d
Realesi89 <ehsan.bavaghar1989@gmail.com>
2025-08-21 17:03:12 +0330

    add pwa

--- resources/views/site/layout.blade.php
+++ resources/views/site/layout.blade.php
@@ -14,8 +14,8 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <link rel="stylesheet" href="{{ asset('site-assets/css/style.css?v=3') }}">
-    @yield('css')
     @laravelPWA
+    @yield('css')
 </head>
 
 <body>
